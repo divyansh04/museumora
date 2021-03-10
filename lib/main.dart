c6c4528bf517e74454f18170d5e4138a44102358
ankitwadhwa615 <ankitwadhwa615@gmail.com>
2021-03-09 23:47:19 -0800

    sign in/up

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:museumora/screens/auth/auth.dart';
+import 'package:museumora/screens/auth/auth_service.dart';
 import 'package:museumora/screens/dashboard/dashboard.dart';
 import 'package:museumora/services/serviceLocator.dart';
-
 import 'config/palette.dart';
 import 'screens/splash.dart';
 import 'services/moduleServices/userServices.dart';
-
+AuthMethods _authMethods=AuthMethods();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //Set rotation
@@ -44,7 +45,16 @@ class MyApp extends StatelessWidget {
         ),
       ),
 
-      home: Dashboard(),
+      home: FutureBuilder(
+        future: _authMethods.getCurrentUser(),
+        builder: (context, AsyncSnapshot<User> snapshot) {
+          if (snapshot.hasData) {
+            return Dashboard();
+          } else {
+            return  AuthScreen();
+          }
+        },
+      ),
       // SplashScreen(),
 
       //home: const SplashScreen(),
--- lib/screens/auth/widgets/register.dart
+++ lib/screens/auth/widgets/register.dart
@@ -1,20 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:museumora/screens/auth/auth_service.dart';
+import 'package:museumora/screens/dashboard/dashboard.dart';
 import 'decoration_functions.dart';
 import 'sign_in_up_bar.dart';
 import 'title.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
   const Register({Key key, this.onSignInPressed}) : super(key: key);
 
   final VoidCallback onSignInPressed;
 
   @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final formKey =GlobalKey<FormState>();
+  AuthMethods _authMethods=AuthMethods();
+  String _email;
+  String _password;
+  bool loading=false;
+  @override
   Widget build(BuildContext context) {
-    // #TODO subitted indicator configuration provider
+
     // final isSubmitting = context.isSubmitting();
-    // #TODO at end check for bugs in Form
-    // #TODO validators in check
+
     return Form(
       child: Padding(
         padding: const EdgeInsets.all(32.0),
@@ -32,54 +43,82 @@ class Register extends StatelessWidget {
             ),
             Expanded(
               flex: 4,
-              child: ListView(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    child: TextFormField(
+              child: Form(
+                key: formKey,
+                child: ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: TextFormField(
+                        onChanged: (value){
+                          _email =value;
+                        },
+                        // ignore: missing_return
+                        validator:(value) {
+                          if (!(value.contains("@")) && value.isNotEmpty) {
+                            return 'please enter a valid email address';
+                          }
+                          if(value==null || value.isEmpty){
+                            return 'please enter email address';
+                          }
+                            },
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                          decoration: registerInputDecoration(hintText: 'Email')),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: TextFormField(
+                        onChanged: (value){
+                          _password =value;
+                        },
+                        // ignore: missing_return
+                        validator:(value) {
+                          if (!(value.length > 5) && value.isNotEmpty) {
+                            return 'Password should contain more than 5 characters';
+                          }
+                          if(value==null || value.isEmpty){
+                            return 'please enter password';
+                          }
+                        },
                         style: const TextStyle(
                           fontSize: 18,
                           color: Colors.white,
                         ),
-                        decoration: registerInputDecoration(hintText: 'Email')),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    child: TextFormField(
-                      style: const TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
+                        decoration: registerInputDecoration(hintText: 'Password'),
                       ),
-                      decoration: registerInputDecoration(hintText: 'Password'),
                     ),
-                  ),
-                  SignUpBar(
-                    label: 'Sign up',
-                    // #TODO subitted indicator configuration provider
-                    isLoading: true, //isSubmitting,
-                    onPressed: () {
-                      // TODO: sign up on pressed
-                      // context.registerWithEmailAndPassword();
-                    },
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: InkWell(
-                      splashColor: Colors.white,
-                      onTap: () {
-                        onSignInPressed?.call();
+                    SignUpBar(
+                      label: 'Sign up',
+                      isLoading: loading, //isSubmitting,
+                      onPressed: () {
+                        if(formKey.currentState.validate()){
+                            performSignUp();
+                        }
+                        // context.registerWithEmailAndPassword();
                       },
-                      child: const Text(
-                        'Sign in',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          decoration: TextDecoration.underline,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: InkWell(
+                        splashColor: Colors.white,
+                        onTap: () {
+                          widget.onSignInPressed?.call();
+                        },
+                        child: const Text(
+                          'Sign in',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            decoration: TextDecoration.underline,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
@@ -87,4 +126,53 @@ class Register extends StatelessWidget {
       ),
     );
   }
+
+  void performSignUp() async {
+    setState(() {
+      loading=true;
+    });
+    try {
+      UserCredential user = await _authMethods.signUp(_email, _password);
+      setState(() {
+        loading=false;
+      });
+      if (user.user != null) {
+        authenticateUser(user.user);
+      }
+    }
+    catch(e){
+      print(e);
+      setState(() {
+        loading=false;
+      });
+    }
+  }
+
+  void authenticateUser(User user) {
+    setState(() {
+      loading=true;
+    });
+    _authMethods.authenticateUser(user).then((isNewUser) {
+      if (isNewUser) {
+        _authMethods.addDataToDb(user).then((value) {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) {
+                return Dashboard();
+              }));
+        });
+        setState(() {
+          loading=false;
+        });
+      } else {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) {
+              return Dashboard();
+            }));
+        setState(() {
+          loading=false;
+        });
+      }
+    });
+  }
 }
+
